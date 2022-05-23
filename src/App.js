79914dd3e3db6eed54dc2832dfa6b720890c0091
boris ya <boris.yarmoulnik@gmail.com>
2022-05-23 18:54:45 +0300

    feat: header and card component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,122 @@
 function App() {
   return (
     <div className="wrapper">
-      React Sneakers
+      <header className="header d-flex justify-between align-center">
+        <div className="d-flex align-center">
+          <img src="/images/logo.svg" width={40} height={40} alt="React Sneakers Shop" />
+
+          <div className="headerInfo ml-15">
+            <h3>React Sheakers</h3>
+            <p>Best sneakers shop</p>
+          </div>
+        </div>
+        <div className="headerRight">
+          <ul className="d-flex align-center clear">
+            <li>
+              <a href="/">
+                <img className="mr-10" src="/images/ico-cart.svg" width={18} height={18} alt="Cart" />
+                <span>$200</span>
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <img src="/images/ico-favorite.svg" width={18} height={18} alt="Cart" />
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <img src="/images/ico-profile.svg" width={18} height={18} alt="Cart" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </header>
+
+      <main className="main">
+        <h1>All Sneakers</h1>
+
+        <div className="card-list">
+          <div className="card">
+            <div>
+              <button className="favorite">
+                <img src="/images/ico-unliked.png" width={32} height={32} alt="Like" />
+              </button>
+
+              <img src="/images/sneakers/1.jpg" alt="" />
+            </div>
+            <h5>Mens Sneakers Nike Blazer Mid Suede</h5>
+            <div className="d-flex align-center justify-between">
+              <div className="d-flex align-center flex-column">
+                <span>Price:</span>
+                <p>$200</p>
+              </div>
+              <button className="buy">
+                <img src="/images/ico-add.svg" width={32} height={32} alt="Add" />
+              </button>
+            </div>
+          </div>
+
+          <div className="card">
+            <div>
+              <button className="favorite">
+                <img src="/images/ico-unliked.png" width={32} height={32} alt="Like" />
+              </button>
+
+              <img src="/images/sneakers/1.jpg" alt="" />
+            </div>
+            <h5>Mens Sneakers Nike Blazer Mid Suede</h5>
+            <div className="d-flex align-center justify-between">
+              <div className="d-flex align-center flex-column">
+                <span>Price:</span>
+                <p>$200</p>
+              </div>
+              <button className="buy">
+                <img src="/images/ico-add.svg" width={32} height={32} alt="Add" />
+              </button>
+            </div>
+          </div>
+
+          <div className="card">
+            <div>
+              <button className="favorite">
+                <img src="/images/ico-unliked.png" width={32} height={32} alt="Like" />
+              </button>
+
+              <img src="/images/sneakers/1.jpg" alt="" />
+            </div>
+            <h5>Mens Sneakers Nike Blazer Mid Suede</h5>
+            <div className="d-flex align-center justify-between">
+              <div className="d-flex align-center flex-column">
+                <span>Price:</span>
+                <p>$200</p>
+              </div>
+              <button className="buy">
+                <img src="/images/ico-add.svg" width={32} height={32} alt="Add" />
+              </button>
+            </div>
+          </div>
+
+          <div className="card">
+            <div>
+              <button className="favorite">
+                <img src="/images/ico-unliked.png" width={32} height={32} alt="Like" />
+              </button>
+
+              <img src="/images/sneakers/1.jpg" alt="" />
+            </div>
+            <h5>Mens Sneakers Nike Blazer Mid Suede</h5>
+            <div className="d-flex align-center justify-between">
+              <div className="d-flex align-center flex-column">
+                <span>Price:</span>
+                <p>$200</p>
+              </div>
+              <button className="buy">
+                <img src="/images/ico-add.svg" width={32} height={32} alt="Add" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
