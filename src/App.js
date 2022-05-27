04a8c62683a2437cfe22a3cb60fe6832d1357084
boris ya <boris.yarmoulnik@gmail.com>
2022-05-27 23:29:56 +0300

    feat: drawer markup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,65 @@
+import Card from "./components/Card";
+
 function App() {
   return (
     <div className="wrapper">
+
+      <div className="drawer-overlay" style={{ display: 'none' }}>
+        <div className="drawer">
+          <h2 className="mb-30 d-flex justify-between align-center">
+            Cart
+            <img src="/images/ico-remove.svg" width={32} height={32} alt="Close cart" />
+          </h2>
+
+          <div className="cart-list mb-30">
+            <div className="cart-item d-flex align-center">
+              <img className="cart-image mr-20" src="/images/sneakers/1.jpg" width={70} height={70} alt="" />
+
+              <div className="mb-5">
+                Mens Sneakers Nike Blazer Mid Suede
+                <p className="mt-5"><b>$200</b></p>
+              </div>
+
+              <button className="remove ml-10">
+                <img src="/images/ico-remove.svg" width={32} height={32} alt="Remove" />
+              </button>
+            </div>
+
+            <div className="cart-item d-flex align-center">
+              <img className="cart-image mr-20" src="/images/sneakers/1.jpg" width={70} height={70} alt="" />
+
+              <div className="mb-5">
+                Mens Sneakers Nike Blazer Mid Suede
+                <p className="mt-5"><b>$200</b></p>
+              </div>
+
+              <button className="remove ml-10">
+                <img src="/images/ico-remove.svg" width={32} height={32} alt="Remove" />
+              </button>
+            </div>
+          </div>
+
+          <ul className="total-block mb-30">
+            <li>
+              <span>Total:</span>
+              <div></div>
+              <b>$200</b>
+            </li>
+            <li>
+              <span>Tax 5%:</span>
+              <div></div>
+              <b>$10</b>
+            </li>
+          </ul>
+
+          <button className="button-green">
+            Place order
+            <img src="/images/ico-arrow.svg" width={14} height={12} alt="Place order" />
+          </button>
+
+        </div>
+      </div>
+
       <header className="header d-flex justify-between align-center">
         <div className="d-flex align-center">
           <img src="/images/logo.svg" width={40} height={40} alt="React Sneakers Shop" />
@@ -35,8 +94,9 @@ function App() {
       <main className="main">
         <div className="d-flex justify-between align-center">
           <h1>All Sneakers</h1>
-          <div className="search-block">
-            search
+          <div className="search-block d-flex align-center justify-center">
+            <img src="/images/ico-search.svg" alt="Search" />
+            <input type="text" placeholder="Search..." />
           </div>
         </div>
 
@@ -121,85 +181,6 @@ function App() {
             </div>
           </div>
 
-          <div className="card">
-            <div>
-              <button className="favorite">
-                <img src="/images/ico-unliked.png" width={32} height={32} alt="Like" />
-              </button>
-
-              <img src="/images/sneakers/1.jpg" alt="" className="card-image" />
-            </div>
-            <h5>Mens Sneakers Nike Blazer Mid Suede</h5>
-            <div className="d-flex align-center justify-between">
-              <div className="d-flex align-center flex-column">
-                <span>Price:</span>
-                <p>$200</p>
-              </div>
-              <button className="buy">
-                <img src="/images/ico-add.svg" width={32} height={32} alt="Add" />
-              </button>
-            </div>
-          </div>
-
-          <div className="card">
-            <div>
-              <button className="favorite">
-                <img src="/images/ico-unliked.png" width={32} height={32} alt="Like" />
-              </button>
-
-              <img src="/images/sneakers/2.jpg" alt="" className="card-image" />
-            </div>
-            <h5>Mens Sneakers Nike Air Max 270</h5>
-            <div className="d-flex align-center justify-between">
-              <div className="d-flex align-center flex-column">
-                <span>Price:</span>
-                <p>$200</p>
-              </div>
-              <button className="buy">
-                <img src="/images/ico-add.svg" width={32} height={32} alt="Add" />
-              </button>
-            </div>
-          </div>
-
-          <div className="card">
-            <div>
-              <button className="favorite">
-                <img src="/images/ico-unliked.png" width={32} height={32} alt="Like" />
-              </button>
-
-              <img src="/images/sneakers/3.jpg" alt="" className="card-image" />
-            </div>
-            <h5>Mens Sneakers Nike Blazer Mid Suede</h5>
-            <div className="d-flex align-center justify-between">
-              <div className="d-flex align-center flex-column">
-                <span>Price:</span>
-                <p>$200</p>
-              </div>
-              <button className="buy">
-                <img src="/images/ico-add.svg" width={32} height={32} alt="Add" />
-              </button>
-            </div>
-          </div>
-
-          <div className="card">
-            <div>
-              <button className="favorite">
-                <img src="/images/ico-unliked.png" width={32} height={32} alt="Like" />
-              </button>
-
-              <img src="/images/sneakers/4.jpg" alt="" className="card-image" />
-            </div>
-            <h5>Mens Sneakers Puma X Aka Boku Future Rider</h5>
-            <div className="d-flex align-center justify-between">
-              <div className="d-flex align-center flex-column">
-                <span>Price:</span>
-                <p>$200</p>
-              </div>
-              <button className="buy">
-                <img src="/images/ico-add.svg" width={32} height={32} alt="Add" />
-              </button>
-            </div>
-          </div>
         </div>
       </main>
     </div>
